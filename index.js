c16adba180edb372d83304780fe3383e56bb9cfd
CCortinas <crcortinas@gmail.com>
2019-10-15 19:23:08 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require("dotenv");
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const axios = require("axios");
+const knex = require("knex");
+
+const server = express();
+
+server.use(cors());
+server.use(helmet());
+server.use(express.json());
+
+server.set("port", process.env.PORT || 8000);
+
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "server is good" });
+});
+
+server.listen(server.get("port"), () => {
+  console.log("=== Listening on port", server.get("port"), " ===");
+});
