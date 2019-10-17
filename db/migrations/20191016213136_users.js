4baaaea67ed0facc21b67a4df4ca52ad91a1bacb
CCortinas <crcortinas@gmail.com>
2019-10-16 22:47:43 -0500

    Added login and register routes. Changed user email to unique

--- db/migrations/20191016213136_users.js
+++ db/migrations/20191016213136_users.js
@@ -1,7 +1,10 @@
 exports.up = function(knex) {
   return knex.schema.createTable("users", tbl => {
     tbl.increments("id");
-    tbl.string("email").notNullable();
+    tbl
+      .string("email")
+      .notNullable()
+      .unique();
     tbl.string("password").notNullable();
   });
 };
--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const cors = require("cors");
 const helmet = require("helmet");
 const axios = require("axios");
 const knex = require("knex");
-const dbConfig = require("./knexfile");
+const dbConfig = require("./knexfile.js");
+const bcrypt = require("bcrypt");
 
 const server = express();
 const db = knex(dbConfig.development);
@@ -17,6 +18,47 @@ server.set("port", process.env.PORT || 8000);
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "server is good" });
+});
+
+server.post("/register", (req, res) => {
+  let newUser = req.body;
+
+  if (newUser.email && newUser.password) {
+    newUser.password = bcrypt.hashSync(newUser.password, 10);
+
+    db("users")
+      .insert(newUser)
+      .then(response => {
+        res.status(201).json(response);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  } else {
+    res.status(400).json({ error: "Request missing one or more parameters" });
+  }
+});
+
+server.post("/login", (req, res) => {
+  const creds = req.body;
+  if (creds.email) {
+    db("users")
+      .where({ email: creds.email })
+      .first()
+      .then(user => {
+        if (user) {
+          if (bcrypt.compareSync(creds.password, user.password)) {
+            res.status(200).json({ message: "Logged in" });
+          } else {
+            res.status(401).json({ message: "Incorrect username or password" });
+          }
+        } else {
+          res.status(401).json({ message: "Incorrect username or password" });
+        }
+      });
+  } else {
+    res.status(401).json({ message: "Incorrect username or password" });
+  }
 });
 
 server.listen(server.get("port"), () => {
